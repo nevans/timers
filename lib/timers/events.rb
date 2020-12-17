a3604b868e07c4289fec5d673c15579ee9ebca1f
nicholas a. evans <nicholas.evans@gmail.com>
2020-12-17 02:57:30 -0500

    [WIP] attempt to performance with a heap (disappointing)
    I cranked up the "range" in the performance_spec by 6x to really see the
    difference.

--- lib/timers/events.rb
+++ lib/timers/events.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 # Copyright, 2018, by Samuel G. D. Williams. <http://www.codeotaku.com>
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -34,6 +34,7 @@ module Timers
 
 			# The absolute time that the handle should be fired at.
 			attr_reader :time
+			alias to_f time
 
 			# Cancel this timer, O(1).
 			def cancel!
@@ -55,8 +56,20 @@ module Timers
 				@time >= other.to_f
 			end
 
-			def to_f
-				@time
+			def <= other
+				@time <= other.to_f
+			end
+
+			def < other
+				@time < other.to_f
+			end
+
+			def == other
+				@time == other.to_f
+			end
+
+			def <=> other
+				@time <=> other.to_f
 			end
 
 			# Fire the callback if not cancelled with the given time parameter.
@@ -66,78 +79,149 @@ module Timers
 		end
 
 		def initialize
-			# A sequence of handles, maintained in sorted order, future to present.
-			# @sequence.last is the next event to be fired.
-			@sequence = []
+			# A min-heap of handles.  @heap[0] is the next event to be fired.
+			@heap = []
 			@queue = []
 		end
 
 		# Add an event at the given time.
 		def schedule(time, callback)
 			handle = Handle.new(time.to_f, callback)
-			
+
 			@queue << handle
-			
+
 			return handle
 		end
 
 		# Returns the first non-cancelled handle.
 		def first
 			merge!
-			
-			while (handle = @sequence.last)
+
+			while handle = heap_peek
 				return handle unless handle.cancelled?
-				@sequence.pop
+				heap_pop
 			end
 		end
 
 		# Returns the number of pending (possibly cancelled) events.
 		def size
-			@sequence.size + @queue.size
+			@heap.size + @queue.size
 		end
 
 		# Fire all handles for which Handle#time is less than the given time.
 		def fire(time)
 			merge!
-			
-			while handle = @sequence.last and handle.time <= time
-				@sequence.pop
+			time = time.to_f
+
+			while handle = heap_pop_lte(time)
 				handle.fire(time)
 			end
 		end
 
 		private
 
+		def heap_peek
+			@heap[0]
+		end
+
+		def heap_pop_lte(time)
+			heap_pop if @heap[0].to_f <= time
+		end
+
+		def heap_pop
+			return if @heap.empty?
+			return @heap.pop if @heap.size == 1
+			popped = @heap[0]
+			heap_delete_at(0)
+			popped
+		end
+
+		# (HEAP_N - 1) / HEAP_N of the heap will be in the bottom layer
+		#
+		# Insert and delete takes O(log n / log d).
+		#
+		# However, if insertions and cancellations are concentrated in the last
+		# layer, they can be done much faster on average.
+		HEAP_N = 4
+
 		def merge!
-			while handle = @queue.pop
+			while handle = @queue.shift
 				next if handle.cancelled?
-				
-				index = bisect_right(@sequence, handle)
-				
-				if current_handle = @sequence[index] and current_handle.cancelled?
-					# puts "Replacing handle at index: #{index} due to cancellation in array containing #{@sequence.size} item(s)."
-					@sequence[index] = handle
-				else
-					# puts "Inserting handle at index: #{index} in array containing #{@sequence.size} item(s)."
-					@sequence.insert(index, handle)
-				end
+				heap_push(handle)
 			end
 		end
 
-		# Return the right-most index where to insert item e, in a list a, assuming
-		# a is sorted in descending order.
-		def bisect_right(a, e, l = 0, u = a.length)
-			while l < u
-				m = l + (u - l).div(2)
-
-				if a[m] >= e
-					l = m + 1
-				else
-					u = m
-				end
-			end
-
-			l
+		# place at the end of the heap and sift up
+		def heap_push(handle)
+			@heap << handle
+			heap_sift_up(@heap.size - 1, handle)
 		end
+
+		# pop the last item to overwrite the deleted item, then sift down
+		def heap_delete_at(index)
+			last = @heap.pop
+			@heap[index] = last
+			heap_sift_down(index, last)
+		end
+
+		def heap_sift_up(index, handle = @heap[index])
+			htime = handle.to_f
+
+			# and sift it up to where it belongs
+			while 0 < index
+				# get parent
+				pindex = (index - 1) / HEAP_N
+				parent = @heap[pindex]
+
+				# parent is smaller: heap is restored
+				break if parent.to_f <= htime
+				# cnt[0] += 1
+
+				# parent is larger: swap and continue sifting
+				@heap[pindex] = handle
+				@heap[index] = parent
+
+				index = pindex
+			end
+			index
+		end
+
+		def heap_sift_down(index, handle = @heap[index])
+			last_index = @heap.size - 1
+			htime = handle.to_f
+
+			# and sift it down to where it belongs
+			while true
+				cindex = cindex0 = (index * HEAP_N) + 1
+				break if last_index < cindex0
+
+				# cnt[0] += 1
+
+				# find the min child (and its cindex)
+				child = @heap[cindex0]
+				ctime = child.to_f
+				i = 1
+				while i < HEAP_N && (sibindex = cindex0 + i) <= last_index
+					sibling = @heap[sibindex]
+					sibtime = sibling.to_f
+					if sibtime < ctime
+						child = sibling
+						ctime = sibtime
+						cindex = sibindex
+					end
+					i += 1
+				end
+
+				# child is larger: heap is restored
+				break if htime <= ctime
+
+				# child is smaller: swap and continue sifting
+				@heap[index] = child
+				@heap[cindex] = handle
+
+				index = cindex
+			end
+		end
+
 	end
 end
